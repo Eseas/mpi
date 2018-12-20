c4396f5481bc4baad6d45f87255b49f04af10b72
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2018-12-20 12:37:04 +0200

    draws mandelbrot successfully

--- test_proj/main.cpp
+++ test_proj/main.cpp
@@ -4,13 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
 #include <unistd.h>
 
 #include <complex>
-
-#include <string>
-
 
 #include <mpi.h>
 
@@ -30,69 +26,66 @@ int
 main(int argc, char **argv)
 {
     MPI_Status	status;
-    int		verbose = 1;
     int		rank;
-    int		np;				/* number of processes in job */
-    int		peer;
-    int		i;
 
-    int     imgWidth = 200;
-    int     imgHeight = 100;
-    int     imgWidthStep = 50;
-    int     imgHeightStep = 50;
+    int		verbose = 1;
+    int		np;				/* number of processes in job */
+    int     maxIter = 30000;
+    int     imgCols = 200;
+    int     imgRows = 100;
+    int     imgColsStep = 10;
+    int     imgRowsStep = 10;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-
-    // Eseas begin
-
-
     if (rank == 0) {
-        int		i;
 
-        int     width = 0;
-        int     height = 0;
-
-        int     totalPartitions = imgWidth / imgWidthStep * imgHeight / imgHeightStep;
+        int     totalPartitions = imgCols / imgColsStep * imgRows / imgRowsStep;
         int     totalPartitionsGathered = 0;
         int     totalPartitionsScatterd = 0;
-
-
 
 
         // create tasks
         int j = 0;
         int** tasks = new int*[totalPartitions];
-        for (int width = 0; width < imgWidth; width += imgWidthStep) {
-            for (int height = 0; height < imgHeight; height += imgHeightStep) {
+        for (int width = 0; width < imgCols; width += imgColsStep) {
+            for (int height = 0; height < imgRows; height += imgRowsStep) {
                 tasks[j] = new int[4];
 
                 tasks[j][0] = width;
                 tasks[j][1] = height;
-                tasks[j][2] = width+imgWidthStep;
-                tasks[j][3] = height+imgHeightStep;
+                tasks[j][2] = width+imgColsStep;
+                tasks[j][3] = height+imgRowsStep;
 
                 printf("Task created. Task: {%d,%d,%d,%d}\n", tasks[j][0], tasks[j][1], tasks[j][2], tasks[j][3]);
 
                 j++;
             }
         }
-        printf("Finished chreating tasks: %d\n", totalPartitions);
+        printf("Finished creating tasks: %d\n", totalPartitions);
 
 
         int **A;
-        A = alloc_2d_int(imgWidth, imgHeight);
+        A = alloc_2d_int(imgRows, imgCols);
 
+        printf("MASTER_INITIAL_IMAGE: \n");
 
-        for (i=1; i<np; i++) {
-
-            for (int a = 0; a < imgWidth; a++) {
-                for (int b = 0; b < imgHeight; b++) {
-                    A[a][b] = 0;
-                }
+        for (int a = 0; a < imgRows; a++) {
+            for (int b = 0; b < imgCols; b++) {
+                A[a][b] = 0;
+                printf("%d", A[a][b]);
             }
+            printf("\n");
+        }
+
+
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~ MASTER INITIALIZING ~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        for (int i = 1; i < np; i++) {
+
             printf("MASTER_preparing_job: Slave_%d\n", i);
             int x0 = tasks[totalPartitionsScatterd][0];
             int y0 = tasks[totalPartitionsScatterd][1];
@@ -100,24 +93,15 @@ main(int argc, char **argv)
             int y1 = tasks[totalPartitionsScatterd][3];
 
             int tsk[4];
-            tsk[0] = tasks[totalPartitionsScatterd][0];
-            tsk[1] = tasks[totalPartitionsScatterd][1];
-            tsk[2] = tasks[totalPartitionsScatterd][2];
-            tsk[3] = tasks[totalPartitionsScatterd][3];
-
-            int area = (x1 - x0) * (y1 - y0);
+            tsk[0] = x0;
+            tsk[1] = y0;
+            tsk[2] = x1;
+            tsk[3] = y1;
 
             MPI_Send(&tsk, 4, MPI_INT, i, 1, MPI_COMM_WORLD);
-            MPI_Send(&(A[tsk[0]][tsk[2]]), area, MPI_INT, i, 1, MPI_COMM_WORLD);
-            printf("MASTER_sent_job: Slave_%d Task: {%d,%d,%d,%d}\n", i, tsk[0], tsk[1], tsk[2], tsk[3]);
+            printf("MASTER_sent_TASK: Slave_%d Task: {%d,%d,%d,%d}\n", i, tsk[0], tsk[1], tsk[2], tsk[3]);
 
             totalPartitionsScatterd++;
-
-//            MPI_Recv(&tsk, 4, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-//            MPI_Recv(&(A[tsk[0]][tsk[2]]), area, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
-//
-//            printf("MASTER_received_job: Slave_%d Task: {%d,%d,%d,%d}\n", status.MPI_SOURCE, tsk[0], tsk[1], tsk[2], tsk[3]);
-
         }
 
 
@@ -126,15 +110,14 @@ main(int argc, char **argv)
         printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
         while (totalPartitionsGathered < totalPartitions) {
 
-
-//            int **A;
-//            A = alloc_2d_int(imgWidth, imgHeight);
-
-
+            int worker;
 
             int tsk[4];
             MPI_Recv(&tsk, 4, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
 
+            worker = status.MPI_SOURCE;
+            int **B;
+            B = alloc_2d_int(imgRowsStep, imgColsStep);
 
             int x0 = tsk[0];
             int y0 = tsk[1];
@@ -145,38 +128,73 @@ main(int argc, char **argv)
             printf("MASTER_received_task: Slave_%d Area: %d Task: {%d,%d,%d,%d}\n", status.MPI_SOURCE, area, tsk[0], tsk[1], tsk[2], tsk[3]);
 
 
-            MPI_Recv(&(A[x0][x0]), area, MPI_INT, status.MPI_SOURCE, 2, MPI_COMM_WORLD, &status);
+            MPI_Recv(&(B[0][0]), area, MPI_INT, status.MPI_SOURCE, 2, MPI_COMM_WORLD, &status);
 
             printf("MASTER_received_job: Slave_%d Area: %d Task: {%d,%d,%d,%d}\n", status.MPI_SOURCE, area, tsk[0], tsk[1], tsk[2], tsk[3]);
 
-//            printf("MASTER_IMAGE: \n");
-//            for (int a = 0; a < imgWidth; a++) {
-//                for (int b = 0; b < imgHeight; b++) {
-//                    printf("%d", A[a][b]);
-//                }
-//                printf("\n");
-//            }
+
+            string masterImage = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nMASTER_RECEIVED_PART\n";
+            for (int a = 0; a < imgRowsStep; a++) {
+                for (int b = 0; b < imgColsStep; b++) {
+                    char buffer[2];
+                    sprintf(buffer, "%d", B[a][b]);
+                    masterImage += buffer;
+                }
+                masterImage += "\n";
+            }
+            cout << masterImage;
+
+
+            string masterImage3 = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nMASTER_IMAGE_BEFORE\n";
+            for (int a = 0; a < imgRows; a++) {
+                for (int b = 0; b < imgCols; b++) {
+                    char buffer[2];
+                    sprintf(buffer, "%d", A[a][b]);
+                    masterImage3 += buffer;
+                }
+                masterImage3 += "\n";
+            }
+            cout << masterImage3;
+            
+            // PUT MATRIX
+            for (int row = y0, a = 0; row < y1 && a < imgRowsStep; row++, a++) {
+                for (int col = x0, b = 0; col < x1 && b < imgColsStep; col++, b++) {
+                    A[row][col] = B[a][b];
+//                    printf("Task: {%d,%d,%d,%d}; %d, %d\n", tsk[0], tsk[1], tsk[2], tsk[3], row, column);
+                }
+            }
+            printf("MATRIX PUT SUCCESSFULLY");
+
+            string masterImage2 = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nMASTER_IMAGE_AFTER\n";
+            for (int a = 0; a < imgRows; a++) {
+                for (int b = 0; b < imgCols; b++) {
+                    char buffer[2];
+                    sprintf(buffer, "%d", A[a][b]);
+                    masterImage2 += buffer;
+                }
+                masterImage2 += "\n";
+            }
+            cout << masterImage2;
+
+            // PUT MATRIX END
+
 
             totalPartitionsGathered++;
 
             if (totalPartitionsScatterd < totalPartitions) {
-                printf("MASTER_preparing_ADDITIONAL_job: Slave_%d\n", i);
                 int x0 = tasks[totalPartitionsScatterd][0];
                 int y0 = tasks[totalPartitionsScatterd][1];
                 int x1 = tasks[totalPartitionsScatterd][2];
                 int y1 = tasks[totalPartitionsScatterd][3];
 
                 int tsk[4];
-                tsk[0] = tasks[totalPartitionsScatterd][0];
-                tsk[1] = tasks[totalPartitionsScatterd][1];
-                tsk[2] = tasks[totalPartitionsScatterd][2];
-                tsk[3] = tasks[totalPartitionsScatterd][3];
+                tsk[0] = x0;
+                tsk[1] = y0;
+                tsk[2] = x1;
+                tsk[3] = y1;
 
-                int area = (x1 - x0) * (y1 - y0);
-
-                MPI_Send(&tsk, 4, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
-                MPI_Send(&(A[tsk[0]][tsk[2]]), area, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
-                printf("MASTER_sent_ADDITIONAL_job: Slave_%d Task: {%d,%d,%d,%d}\n", i, tsk[0], tsk[1], tsk[2], tsk[3]);
+                MPI_Send(&tsk, 4, MPI_INT, worker, 1, MPI_COMM_WORLD);
+                printf("MASTER_preparing_ADDITIONAL_job: Slave_%d: {%d,%d,%d,%d}\n", worker, tsk[0], tsk[1], tsk[2], tsk[3]);
 
                 totalPartitionsScatterd++;
             }
@@ -184,114 +202,61 @@ main(int argc, char **argv)
         }
 
 
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+               "~~~~~~~~~~~~~~~~~~~~~~~~ MASTER EXIT ~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+               "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
     } else if (rank != 0) {
         while(1) {
-            int **A;
             int **B;
-            A = alloc_2d_int(imgWidth, imgHeight);
-
-
-
-            for (int a = 0; a < 10; a++) {
-                for (int b = 0; b < 10; b++) {
-                    A[a][b] = 0;
-                }
-            }
-
             int tsk[4];
-
             int area;
 
-
-
             MPI_Recv(&tsk, 4, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-            printf("SLAVE_%d_received_task: {%d,%d,%d,%d}\n", rank, tsk[0], tsk[1], tsk[2], tsk[3]);
+            printf("SLAVE_%d_received_TASK: {%d,%d,%d,%d}\n", rank, tsk[0], tsk[1], tsk[2], tsk[3]);
 
             int x0 = tsk[0];
             int y0 = tsk[1];
             int x1 = tsk[2];
             int y1 = tsk[3];
 
-            B = alloc_2d_int(x1-x0,x1-x0);
+            B = alloc_2d_int(imgRowsStep,imgColsStep);
 
             area = (x1 - x0) * (y1 - y0);
             printf("Area: %d\n", area);
 
-            MPI_Recv(&(B[0][0]), area, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-//            MPI_Recv(&(B[x0][y0]), area, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
 
-            printf("Received\n");
-
-//        MPI_Send(&tsk, 4, MPI_INT, 0, 1, MPI_COMM_WORLD);
-//        MPI_Send(&tsk, 4, MPI_INT, 0, 1, MPI_COMM_WORLD);
             string slaveJob1 = "";
-            for (int row = x0; row < x1; row++) {
-                for (int column = y0; column < y1; column++) {
-//                printf("%c", A[a][b]);
+            for (int row = y0, a = 0; row < y1; row++, a++) {
+                for (int col = x0, b = 0; col < x1; col++, b++) {
+
+//                printf("Received: %d, %d\n", row, column);
 //                    B[a][b] = rank;
                     std::complex<float> z, c = {
 //                            (float)column * 2 / y1 - 1.5f,
 //                            (float)row * 2 / x1 - 1
-//                            column,
-                            ((float)column - imgHeight/2) / (imgHeight/2),
-                    ((float)row - imgWidth/2) / (imgWidth/4)
+                            ((float)col - imgCols/2) / (imgCols/4),
+                            ((float)row - imgRows/2) / (imgRows/2)
                     };
                     int iteration = 0;
-                    while(abs(z) < 2 && ++iteration < 100)
+                    while(abs(z) < 2 && ++iteration < maxIter)
                         z = pow(z, 2) + c;
-//                    std::cout << (iteration == 100 ? '#' : '.');
-                    slaveJob1 +=  (iteration == 100 ? '#' : '.');
+                    slaveJob1 +=  (iteration == maxIter ? '#' : '.');
+                    B[a][b] = (iteration == maxIter ? 1 : 0);
                 }
-//                printf("\n");
                 slaveJob1 += "\n";
             }
 
-            cout << slaveJob1;
-//
-//            for (int a = x0; a < x1; a++) {
-//                for (int b = y0; b < y1; b++) {
-////                printf("%c", A[a][b]);
-//                    B[a][b] = rank;
-//                }
-//            }
+//            cout << slaveJob1;
 
-//        for (int a = 0; a < 5; a++) {
-//            for (int b = 0; b < 5; b++) {
-//                A[a][b] = '?';
-//            }
-//        }
-//            string slaveJob = "";
-//            printf("Slave made: imgWidth: %d\n", imgWidth);
-//            for (int a = 0; a < x1-x0; a++) {
-//                for (int b = 0; b < y1-y0; b++) {
-//                    B[a][b] = rank;
-////                    printf("%d", B[a][b]);
-//                    char buffer[2];
-//                    sprintf(buffer, "%d", B[a][b]);
-//                    slaveJob += buffer;
-//                }
-//                slaveJob += "\n";
-//            }
-
-//            cout << slaveJob;
-            printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-
-//            printf("Slave made: imgWidth: %d\n", imgWidth);
-//            for (int a = 0; a < imgWidth; a++) {
-//                for (int b = 0; b < imgHeight; b++) {
-//                    printf("%d", A[a][b]);
-//                }
-//                printf("\n");
-//            }
 
             MPI_Send(&tsk, 4, MPI_INT, 0, 1, MPI_COMM_WORLD);
 
-            printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            printf("##############################################\n");
             printf("Slave sent its task back\n");
-            printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            printf("##############################################\n");
 
             MPI_Send(&(B[0][0]), area, MPI_INT, 0, 2, MPI_COMM_WORLD);
-//            MPI_Send(&(B[3][0]), 20, MPI_INT, 0, 2, MPI_COMM_WORLD);
 
             printf("SLAVE_%d_submitted_job: {%d,%d,%d,%d}\n", rank, tsk[0], tsk[1], tsk[2], tsk[3]);
         }
